f6d4098deab56532eac221d5d555a42dd78bf7ae
성현제 <hyunjaesung@seonghyeonje.local>
2020-06-21 17:38:13 +0900

    Enable Django admn for user profile

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xrn+#+3jq0*kclh$)u%=c%ky=wym&0n)jj3%r2k#v*@@3@3+hr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
